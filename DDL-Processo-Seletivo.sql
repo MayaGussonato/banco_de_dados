bc27ed96a09df60bebe7a68b1d97408e8f52a7e8
Mayara Gussonato <maygussonato@gmail.com>
2025-10-07 08:12:39 -0300

    modificacao

--- DDL-Processo-Seletivo.sql
+++ DDL-Processo-Seletivo.sql
@@ -1,50 +1,65 @@
-CREATE DATABASE db_devconnect;
+CREATE DATABASE DevConnect;
+GO
 
-USE db_devconnect;
+USE DevConnect;
+GO
 
+--DDL
 CREATE TABLE tb_usuario(
-id            INT IDENTITY	(1,1) PRIMARY KEY,
-nomeCompleto  NVARCHAR	    (255) NOT NULL,
-nomeDeUsuario NVARCHAR	    (20)  UNIQUE   NOT NULL,
-email         NVARCHAR	    (255) UNIQUE   NOT NULL,
-senha         NVARCHAR	    (50)  NOT NULL,
-fotoPerfilUrl NVARCHAR      (255)
+id                  INT IDENTITY(1, 1) PRIMARY KEY,
+nome_completo       NVARCHAR(255)      NOT NULL,
+nome_usuario        NVARCHAR(50)       UNIQUE NOT NULL,
+email               NVARCHAR(255)      UNIQUE NOT NULL,
+senha               NVARCHAR(30)       NOT NULL,
+foto_perfil_url     NVARCHAR(150)      NULL
 );
 
 SELECT * FROM tb_usuario;
 
+CREATE TABLE tb_seguidor(
+    id            INT IDENTITY(1, 1) PRIMARY KEY,
+    usuarioid     NVARCHAR(255)      NOT NULL, -- usuario que esta sendo seguido
+    seguidorid    NVARCHAR(255)      NOT NULL, -- usuario que esta seguindo
+
+    CONSTRAINT fk_seguidor_usuarioid  FOREIGN KEY (usuarioid)  REFERENCES tb_usuario(id),
+    CONSTRAINT fk_seguidor_seguidorid FOREIGN KEY (seguidorid) REFERENCES tb_usuario(id)
+);
+GO
+
+SELECT * FROM tb_seguidor;
+
 CREATE TABLE tb_publicacao(
-id              INT IDENTITY	(1,1) PRIMARY KEY,
-descricao	    NVARCHAR		(500),
-imagemUrl       NVARCHAR	    (255) NOT NULL,
-data_Publicacao DATE NOT NULL,
-id_Usuario      INT NOT NULL FOREIGN KEY REFERENCES tb_usuario(id)
+id              INT IDENTITY(1, 1) PRIMARY KEY,
+usuarioid       INT                NOT NULL,	
+descricao       NVARCHAR(255)      NOT NULL,
+imagem_url      NVARCHAR(150)      NOT NULL,
+data_publicacao DATE               NOT NULL,
+
+CONSTRAINT fk_usuario FOREIGN KEY (usuarioid) REFERENCES tb_usuario(id)
 );
 
 SELECT * FROM tb_publicacao;
 
 CREATE TABLE tb_curtida(
-id              INT IDENTITY (1,1)   PRIMARY KEY,
-id_Usuario      INT NOT NULL FOREIGN KEY REFERENCES tb_usuario(id),
-id_Publicacao	INT NOT NULL FOREIGN KEY REFERENCES tb_publicacao(id)
+id            INT IDENTITY(1, 1) PRIMARY KEY,
+usuarioid     INT NOT NULL,
+publicacaoid  INT NOT NULL,
+
+CONSTRAINT fk_usuario_ca    FOREIGN KEY (usuarioid)    REFERENCES tb_usuario(id),
+CONSTRAINT fk_publicacao_ca FOREIGN KEY (publicacaoid) REFERENCES tb_publicacao(id)
 );
 
 SELECT * FROM tb_curtida;
 
 CREATE TABLE tb_comentario(
-id              INT IDENTITY (1,1) PRIMARY KEY,
-texto           NVARCHAR (255) NOT NULL,
-dataComentario  DATE NOT NULL,
-id_Publicacao	INT NOT NULL FOREIGN KEY REFERENCES tb_publicacao(id),
-id_Usuario      INT NOT NULL FOREIGN KEY REFERENCES tb_usuario(id)
+id               INT IDENTITY(1, 1) PRIMARY KEY,
+usuarioid        INT NOT NULL,
+publicacaoid     INT NOT NULL,
+texto            NVARCHAR(255) NOT NULL,
+data_comentario  DATE NOT NULL,
+
+CONSTRAINT fk_usuario_ct    FOREIGN KEY (usuarioid) REFERENCES tb_usuario(id),
+CONSTRAINT fk_publicacao_ct FOREIGN KEY (publicacaoid) REFERENCES tb_publicacao(id)
 );
 
 SELECT * FROM tb_comentario;
-
-CREATE TABLE tb_seguidor(                   
-id_Usuario_Seguir  INT NOT NULL,
-id_Usuario_Seguido INT NOT NULL,
-
-PRIMARY KEY (id_Usuario_Seguir, id_Usuario_Seguido)
-);
-SELECT * FROM tb_seguidor;
